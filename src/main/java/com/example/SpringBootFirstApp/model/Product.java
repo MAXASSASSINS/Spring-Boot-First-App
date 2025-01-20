88c11b02c1eea520eb64401581aa6c1a58665f47
MAXASSASSINS <mohammedshadab23april@gmail.com>
2025-01-20 23:40:04 +0530

    add props for image

--- src/main/java/com/example/SpringBootFirstApp/model/Product.java
+++ src/main/java/com/example/SpringBootFirstApp/model/Product.java
@@ -1,10 +1,7 @@
 package com.example.SpringBootFirstApp.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,4 +24,9 @@ public class Product {
     private Date release_date;
     private int quantity;
     private boolean available;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 }
